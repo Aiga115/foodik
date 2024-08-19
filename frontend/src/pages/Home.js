823307688b481cbcb6af89bd8d415788895ba0e3
Aiga115 <aiguljhyldyzbekkyzy@gmail.com>
2024-08-19 11:45:26 +0200

    changes made

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 const Home = () => {
     return (
         <div style={{ height: '100vh', textAlign: 'center' }}>
-            <h1>Foodik's Menu</h1>
         </div>
     )
 }
