d59c16e99d33bba6714ae75eb206fb230dcf55a6
sana188 <akmatbekova18@gmail.com>
2024-08-21 17:05:32 +0600

    changes made

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import './Card.css';
-import DishIcon from '../assets/dish.png';
+import React from "react";
+import DishIcon from "../assets/dish.png";
+
+
 const Card = ({ name, price, onAddToBasket }) => {
   return (
-    <div className="card">
-      <img src={DishIcon} alt={name} />
-      <h3>{name}</h3>
-      <p>${price.toFixed(2)}</p>
-      <button onClick={onAddToBasket}>Add to Basket</button>
-    </div>
+    <form action="" method="post" class="box">
+      <input type="hidden" name="pid" value="" />
+      <input
+        type="hidden"
+        name="name"
+        value="<?= $fetch_products['name']; ?>"
+      />
+      <input
+        type="hidden"
+        name="price"
+        value="<?= $fetch_products['price']; ?>"
+      />
+      <input
+        type="hidden"
+        name="image"
+        value="<?= $fetch_products['image']; ?>"
+      />
+      <a
+        href="quick_view.php?pid=<?= $fetch_products['id']; ?>"
+        class="fas fa-eye"
+      ></a>
+      <button
+        type="submit"
+        class="fas fa-shopping-cart"
+        name="add_to_cart"
+      ></button>
+      <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="" />
+      <a
+        href="category.php?category=<?= $fetch_products['category']; ?>"
+        class="cat"
+      ></a>
+      <div class="name"></div>
+      <div class="flex">
+        <div class="price">
+          <span>$</span>
+        </div>
+        <input
+          type="number"
+          name="qty"
+          class="qty"
+          min="1"
+          max="99"
+          value="1"
+          maxlength="2"
+        />
+      </div>
+    </form>
   );
 };
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,38 @@
 import React from "react";
 import Tabs from "../components/Tabs";
 import Sidebar from "../components/Sidebar";
-
+import Card from "../components/Card";
 
 const Home = () => {
-    return (
-        <div style={{ height: '100vh', textAlign: 'center', width: '100%', position: "relative" }}>
-            <Sidebar />
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "start", marginLeft: "350px" }}>
-                <Tabs activeItem={"Salad"} />
-                <div></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        height: "100vh",
+        textAlign: "center",
+        width: "100%",
+        position: "relative",
+      }}
+    >
+      <Sidebar />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "start",
+          marginLeft: "250px"
+        }}
+      >
+        <Tabs activeItem={"Salad"} />
+        <section className="products" style={{ margin: 0 }}>
+          <div className="box-container" style={{ gridTemplateColumns: "auto auto auto" }}>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 export default Home;
